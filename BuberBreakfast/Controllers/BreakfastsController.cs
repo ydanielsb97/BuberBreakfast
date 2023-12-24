e6e64e740ddfcc609603fe87953bc9d51723f7c2
Yeison Serrano <ydanielsb97@gmail.com>
2023-12-24 17:09:28 -0400

    ⚡️ Adding logic to the controllers, refactoring routes and using Rest Client extension

--- BuberBreakfast/Controllers/BreakfastsController.cs
+++ BuberBreakfast/Controllers/BreakfastsController.cs
@@ -5,28 +5,29 @@ using Microsoft.AspNetCore.Mvc;
 namespace BuberBreakfast.Controllers;
 
 [ApiController]
+[Route("[controller]")]
 public class BreakfastsController : ControllerBase
 {
 
-    [HttpPost("/breakfasts")]
+    [HttpPost()]
     public IActionResult CreateBreakfast(CreateBreakfastRequest request)
     {
         return Ok(request);
     }
 
-    [HttpGet("/breakfasts/{id:guid}")]
+    [HttpGet("/{id:guid}")]
     public IActionResult GetBreakfast(Guid id)
     {
         return Ok(id);
     }
 
-    [HttpPut("/breakfasts/{id:guid}")]
+    [HttpPut("/{id:guid}")]
     public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request)
     {
         return Ok(request);
     }
 
-    [HttpDelete("/breakfasts/{id:guid}")]
+    [HttpDelete("/{id:guid}")]
     public IActionResult DeleteBreakfast(Guid id)
     {
         return Ok(id);
